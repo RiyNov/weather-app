11196d83bd313422eb7b1ee4d0672d85a7ee3b70
Riyandi Novian <43406693+RiyNov@users.noreply.github.com>
2019-07-18 15:28:57 +0700

    First push

---
+++ precache-manifest.535e6afa7e8f29df6753729fc56df07c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c112e8a957cee7052369c83683539063",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55b6474d13403c5d51a6",
+    "url": "/static/css/2.4ee331c5.chunk.css"
+  },
+  {
+    "revision": "6618ce602d07048a2f4b",
+    "url": "/static/css/main.fd054b40.chunk.css"
+  },
+  {
+    "revision": "55b6474d13403c5d51a6",
+    "url": "/static/js/2.77ce8605.chunk.js"
+  },
+  {
+    "revision": "6618ce602d07048a2f4b",
+    "url": "/static/js/main.b26eca84.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
